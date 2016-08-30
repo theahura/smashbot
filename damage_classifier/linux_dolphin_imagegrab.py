f651249f8276728e95412549bccaa2dd7f61678a
Amol Kapoor <theahura@gmail.com>
2016-08-30 04:38:37 -0400

    Cleaned up imagegrab.

--- damage_classifier/linux_dolphin_imagegrab.py
+++ damage_classifier/linux_dolphin_imagegrab.py
@@ -6,16 +6,16 @@ Usage: open dolphin with melee, start program immediately after round starts,
 kill program immediately after round ends.
 """
 
-import gtk.gdk
-import numpy as np
 import time
 
-win = gtk.gdk.get_default_root_window()
-w, h = win.get_size()
-pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB,False,8,w,h)
+import gtk.gdk
+
+WIN = gtk.gdk.get_default_root_window()
+W, H = WIN.get_size()
+pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, W, H)
 
 for i in xrange(300):
-    pb = pb.get_from_drawable(win,win.get_colormap(),0,0,0,0,w,h)
+    pb = pb.get_from_drawable(WIN, WIN.get_colormap(), 0, 0, 0, 0, W, H)
     pb.save("%s.png" % str(i), "png")
 
     time.sleep(1)
