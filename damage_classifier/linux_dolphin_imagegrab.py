88891f0a3f4ce7b8ceac5e6b23a2f2cadeb77803
Amol Kapoor <theahura@gmail.com>
2016-09-07 00:43:30 -0400

    Added schedule

--- damage_classifier/linux_dolphin_imagegrab.py
+++ damage_classifier/linux_dolphin_imagegrab.py
@@ -11,13 +11,18 @@ import time
 import gtk.gdk
 import classifier_constants as cc
 
+START_NAME = 2639
+
 WIN = gtk.gdk.get_default_root_window()
 
-pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, cc.WIDTH, cc.HEIGHT)
+pb = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, cc.GATHER_WIDTH,
+                    cc.GATHER_HEIGHT)
 
 for i in xrange(cc.NUM_IMAGE_GRAB):
     pb = pb.get_from_drawable(WIN, WIN.get_colormap(), 0, 0, 0, 0,
-            cc.WIDTH, cc.HEIGHT)
-    pb.save("%s/%s.png" % (cc.PATH_TO_DATA, str(i)), "png")
+            cc.GATHER_WIDTH, cc.GATHER_HEIGHT)
+    pb.save("%s/%s.png" % (cc.PATH_TO_DATA, str(i + START_NAME)), "png")
 
-    time.sleep(1)
+    print "captured image: %s" % str(i + START_NAME)
+
+    time.sleep(0.3)
